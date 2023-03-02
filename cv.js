00cacf5fd7ffb2d792a2f156b66428946ddb7019
raul23080111 <113809499+raul23080111@users.noreply.github.com>
2023-03-02 16:50:31 -0300

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,9 @@
+document.getElementById("boton1").onclick = function () {
+    
+    document.getElementById("demo").innerHTML = "Otros trabajos como instructor de gimnasios de musculacion";
+}
+
+document.getElementById("boton2").addEventListener("click", function() {
+    
+    document.getElementById("estudios").innerHTML = "Capacitaciones en administracion contable y en programacion html, css y js"
+});
